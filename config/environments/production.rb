0bb1837f5aa098821c5eb0b16147dba16e2169d1
pezholio <pezholio@gmail.com>
2017-11-09 09:57:37 +0000

    Add Mailgun config to production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,6 +85,16 @@ Rails.application.configure do
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
+  
+  ActionMailer::Base.smtp_settings = {
+    port: ENV['MAILGUN_SMTP_PORT'],
+    address: ENV['MAILGUN_SMTP_SERVER'],
+    user_name: ENV['MAILGUN_SMTP_LOGIN'],
+    password: ENV['MAILGUN_SMTP_PASSWORD'],
+    domain: 'skills-and-innovation.herokuapp.com',
+    authentication: :plain
+  }
+  ActionMailer::Base.delivery_method = :smtp
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
