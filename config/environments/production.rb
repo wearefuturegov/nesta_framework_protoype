0ede15c5f74c6dfdc3e75b302db0c068c5df180d
pezholio <pezholio@gmail.com>
2017-11-12 20:14:48 +0000

    Switch to Sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,12 +87,13 @@ Rails.application.configure do
   end
   
   ActionMailer::Base.smtp_settings = {
-    port: ENV['MAILGUN_SMTP_PORT'],
-    address: ENV['MAILGUN_SMTP_SERVER'],
-    user_name: ENV['MAILGUN_SMTP_LOGIN'],
-    password: ENV['MAILGUN_SMTP_PASSWORD'],
-    domain: 'skills-and-innovation.herokuapp.com',
-    authentication: :plain
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'wearefuturegov.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
   }
   ActionMailer::Base.delivery_method = :smtp
 
