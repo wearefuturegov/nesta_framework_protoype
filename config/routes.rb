9447d430798fa444598c6684a501773e518ef2b3
Chris Evans <evanschris@users.noreply.github.com>
2017-11-16 16:47:44 +0000

    add in privacy page and fg logo into footer

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'home/index'
-  get 'home/about'
+  get 'home/privacy'
 
   get 'assess/step1'
   get 'assess/step2'
@@ -8,18 +8,18 @@ Rails.application.routes.draw do
   get 'assess/step4'
   get 'assess/step5'
   get 'assess/step6'
-  
+
   resources :assessments do
     collection do
       get :areas, action: :areas
     end
     resources :teams, only: [:new, :create]
   end
-  
+
   resources :teams do
     resources :assessments, only: :index
   end
-  
+
   resources :users do
     member do
       get :start_assessment, action: :start_assessment
