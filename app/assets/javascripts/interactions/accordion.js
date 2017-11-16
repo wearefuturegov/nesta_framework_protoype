5f56cb72b12213657dacbca43e7fb5f38d071b55
Chris Evans <evanschris@users.noreply.github.com>
2017-11-16 15:59:22 +0000

    fix accordion bug

--- app/assets/javascripts/interactions/accordion.js
+++ app/assets/javascripts/interactions/accordion.js
@@ -4,12 +4,18 @@ $.fn.accordion = function() {
   $(this).click(function() {
     var $this = $(this);
     if ($this.parent().hasClass('open')) {
+      if($this.parent().hasClass('parent_accord')) {
+        $('.accordion').removeClass('open');
+        $('.accord_arrow').removeClass('rotate');
+      }
       $this.parent().removeClass('open');
       $this.find('.accord_arrow').removeClass('rotate');
       $this.next().slideUp(500);
+
     } else {
       $this.parent().parent().find('.accord_content').removeClass('open');
       $this.parent().parent().find('.accord_content').slideUp(500);
+      $this.parent().parent().find('.accord_arrow').removeClass('rotate');
       $this.parent().toggleClass('open');
       $this.find('.accord_arrow').addClass('rotate');
       $this.next().slideToggle(500);
