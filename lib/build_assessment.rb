949ce1bf111de7169326c077820a9fcfa850e2aa
pezholio <pezholio@gmail.com>
2017-11-16 17:50:53 +0000

    Add colours to areas
    So they can be easily reference in the pie charts

--- lib/build_assessment.rb
+++ lib/build_assessment.rb
@@ -5,6 +5,7 @@ class BuildAssessment
     yaml['areas'].each do |a|
       area = Area.find_or_create_by(name: a['name'])
       area.description = a['description']
+      area.colour = a['colour']
       area.save
       a['skills'].each do |s|
         skill = Skill.find_or_create_by(name: s['name'], area: area)
